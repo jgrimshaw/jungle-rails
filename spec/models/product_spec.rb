93b2bf9c4e368af0520a47b3db011f15b13a97a7
Jelena Grimshaw <muchstudio@hotmail.com>
2019-06-17 21:44:16 +0000

    feature/product-specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,30 +16,25 @@ RSpec.describe Product, type: :model do
     it 'is not valid without a name' do
       @product.name = nil
       @product.save
-      # expect(@product.errors.messages).to include name: ["can't be blank"]
       expect(@product.errors.full_messages).to include "Name can't be blank"
     end
 
     it 'is not valid without a price' do
       @product.price_cents = nil
       @product.save
-      # expect(@product.errors.messages).to include name: ["can't be blank"]
       expect(@product.errors.full_messages).to include "Price can't be blank"
     end
 
     it 'is not valid without a quantity' do
       @product.quantity = nil
       @product.save
-      # expect(@product.errors.messages).to include name: ["can't be blank"]
       expect(@product.errors.full_messages).to include "Quantity can't be blank"
     end
 
     it 'is not valid without a category_id' do
       @product.category = nil
       @product.save
-      # expect(@product.errors.messages).to include name: ["can't be blank"]
       expect(@product.errors.full_messages).to include "Category can't be blank"
     end
   end
-
 end
